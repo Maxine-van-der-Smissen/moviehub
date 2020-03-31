177770cd5e487fba7e403f43cf15891a4937bc47
InstantlyMoist <kyllian007@gmail.com>
2020-03-31 16:51:55 +0200

    basic account saving

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -1,3 +1,7 @@
+import 'dart:html';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Account {
   final int accountId;
   final int sessionId;
@@ -5,4 +9,22 @@ class Account {
   final String profileURL;
 
   Account(this.accountId, this.sessionId, this.username, this.profileURL);
+
+  void saveSessionID() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setString("account", this.toString());
+  }
+
+   Future<Account> fromJson() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String accountString = preferences.getString("account");
+    if (accountString == null || accountString.isEmpty) return null;
+    var jsonString = json.decode(accountString);
+    return Account(jsonString['accountId'] as int, jsonString['sessionId'] as int, jsonString['username'] as String, jsonString['profileURL'] as String);
+  }
+
+  @override
+  String toString() {
+    return '{ ${this.accountId}, ${this.sessionId}, ${this.username}, ${this.profileURL} }';
+  }
 }
